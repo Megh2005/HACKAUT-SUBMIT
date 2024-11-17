fadf9a0be01dc01c6dca792f238a0c53cb2b1f62
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2024-11-17 21:59:58 +0530

    update timeline titles and content, and replace images with new URLs

--- gurudev-landing/app/explore/page.tsx
+++ gurudev-landing/app/explore/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function TimelineDemo() {
   const data = [
     {
-      title: "Step - 1",
+      title: "Analysis",
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-lg font-bold mb-8">
@@ -31,7 +31,7 @@ export default function TimelineDemo() {
       ),
     },
     {
-      title: "Step - 2",
+      title: "Scorings",
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-lg font-bold mb-8">
@@ -58,23 +58,23 @@ export default function TimelineDemo() {
       ),
     },
     {
-      title: "Changelog",
+      title: "Resolution",
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-lg font-bold mb-8">
-            Get Proper Scoring For Your Resume
+            Suggestions & platform redirection to improve your skills
           </p>
           <div className="mb-8">
             <div className="grid grid-cols-2 gap-4">
               <Image
-                src="https://assets.aceternity.com/pro/hero-sections.png"
+                src="https://res.cloudinary.com/djyk287ep/image/upload/v1731860935/5_lehgff.jpg"
                 alt="hero template"
                 width={500}
                 height={500}
                 className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
               />
               <Image
-                src="https://assets.aceternity.com/features-section.png"
+                src="https://res.cloudinary.com/djyk287ep/image/upload/v1731860935/6_legd8w.jpg"
                 alt="feature template"
                 width={500}
                 height={500}
