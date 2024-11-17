a2b706219ea39951808cdfd3c5bd5a8152edb12d
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2024-11-17 23:13:25 +0530

    refactor welcome message and update "Find Internship" link styling in the main page

--- gurudev-landing/app/page.tsx
+++ gurudev-landing/app/page.tsx
@@ -14,9 +14,6 @@ export default function BackgroundBeamsWithCollisionDemo() {
             Want To Educate Yourself With Your Passion ?
             <br />
             <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-              {/* <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-                <span className="">Welcome To Gurudev</span>
-              </div> */}
               <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
                 <span className="">Welcome To Gurudev</span>
               </div>
@@ -29,7 +26,11 @@ export default function BackgroundBeamsWithCollisionDemo() {
                 Explore More
               </div>
             </Link>
-            <Link href={"http://localhost:5173"} target="_blank" className="p-[3px] relative mt-6">
+            <Link
+              href={"http://localhost:5173"}
+              target="_blank"
+              className="p-[3px] relative mt-6"
+            >
               <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
               <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-xl font-extrabold text-white hover:bg-transparent">
                 Find Internship
